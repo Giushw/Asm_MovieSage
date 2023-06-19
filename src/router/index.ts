bde0c564a7efc95725bcd5806847740de79d50f7
Giushw <dellavvocatog@gmail.com>
2023-06-19 09:17:28 +0200

    chore(): remove unused route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import MainPageVue from '@/Views/MainPage.vue';
-// import AboutViewVue from '@Views/AboutView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +9,6 @@ const router = createRouter({
       name: 'home',
       component: MainPageVue
     },
-    // {
-    //   path: '/movie/:id',
-    //   name: 'movie',
-    //   component: () => AboutViewVue
-    // }
   ]
 })
 
